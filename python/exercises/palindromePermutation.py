dda8e7b3b81126fb6e450356b6c50bb83a3331c1
RakaPKS <raka@rpkschipperheijn.com>
2021-03-18 17:41:11 +0100

    added python palindromePermutation

---
+++ python/exercises/palindromePermutation.py
@@ -0,0 +1,30 @@
+# Given a string s, write a function to check if it is a permutation of a palindrome.
+# A palindrome is a word or phrase that is the same forwards and backwards.
+# A permutation is a rearrangement of letters. The palindrome does not need to be limited
+# to just dictionary words.
+
+
+def PalindromePermutation(s):
+    s = s.lower()
+    countLetter = {}
+    for character in s:
+        if ord(character) >= 97 and ord(character) <= 122:
+            if character in countLetter.keys():
+                countLetter[character] += 1
+            else:
+                countLetter[character] = 1
+    middle = ""
+    for character in countLetter:
+        if middle and countLetter[character] % 2 == 1:
+            return False
+        if countLetter[character] % 2 == 1:
+            middle = character
+    return True
+
+
+trueS = "Taco cat"
+falseS = "world"
+# Should return True
+print(PalindromePermutation(trueS))
+# Should return false
+print(PalindromePermutation(falseS))
