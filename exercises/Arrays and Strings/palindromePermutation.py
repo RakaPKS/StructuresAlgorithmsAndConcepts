71b180e0cc90ee0c2132d3ec883940c560f25bd0
RakaPKS <raka@rpkschipperheijn.com>
2021-03-18 17:56:00 +0100

    formatted palindromePermutation.py

--- exercises/Arrays and Strings/palindromePermutation.py
+++ exercises/Arrays and Strings/palindromePermutation.py
@@ -3,9 +3,7 @@
 # A permutation is a rearrangement of letters. The palindrome does not need to be limited
 # to just dictionary words.
 
-
-def PalindromePermutation(s):
-    s = s.lower()
+def createCountDict(s):
     countLetter = {}
     for character in s:
         if ord(character) >= 97 and ord(character) <= 122:
@@ -13,6 +11,10 @@ def PalindromePermutation(s):
                 countLetter[character] += 1
             else:
                 countLetter[character] = 1
+    return countLetter
+
+
+def checkPalindromePerm(countLetter):
     middle = ""
     for character in countLetter:
         if middle and countLetter[character] % 2 == 1:
@@ -20,6 +22,12 @@ def PalindromePermutation(s):
         if countLetter[character] % 2 == 1:
             middle = character
     return True
+
+
+def PalindromePermutation(s):
+    s = s.lower()
+    countLetter = createCountDict(s)
+    return checkPalindromePerm(countLetter)
 
 
 trueS = "Taco cat"
